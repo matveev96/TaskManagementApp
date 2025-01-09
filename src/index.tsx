30d8e2750429d66be9d96ba8c6cafbf4c65aa533
matveev96 <sasha.matveev1996@icloud.com>
2025-01-09 12:23:08 +0300

    sprint 3 week 2: wrap App in Provider with store from react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from "./app/App";
+import {Provider} from "react-redux";
+import {store} from "./app/store";
 
-
-const root = ReactDOM.createRoot(
+const root = createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
