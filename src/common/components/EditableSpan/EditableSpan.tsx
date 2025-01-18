6dca5fae63e993bb780faa1cd7135e8e41564246
matveev96 <sasha.matveev1996@icloud.com>
2025-01-18 08:30:31 +0300

    sprint 3 week 3: in common folder add components with AddItemForm, EditableSpan, Header, MenuButton

---
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,38 @@
+import React, {ChangeEvent, useState} from 'react';
+import TextField from '@mui/material/TextField';
+
+type EditableSpanPropsType = {
+    oldTitle: string
+    onClick: (updateTitle: string) => void
+}
+
+export const EditableSpan = ({oldTitle, onClick}: EditableSpanPropsType) => {
+
+    const [edit, setEdit] = useState(false)
+    const [updateTitle, setUpdateTitle] = useState(oldTitle)
+
+    const editHandler = () => {
+        setEdit(!edit)
+        if (edit) onClick(updateTitle)
+    }
+
+    const updateTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setUpdateTitle(event.currentTarget.value)
+
+    }
+
+    return (
+        edit ?
+            <TextField value={updateTitle}
+                       autoFocus
+                       variant="standard"
+                       helperText="Enter changes"
+                       onBlur={editHandler}
+                       onChange={updateTitleHandler}
+
+
+            />
+            :
+            <span onDoubleClick={editHandler}>{oldTitle}</span>
+    );
+};
