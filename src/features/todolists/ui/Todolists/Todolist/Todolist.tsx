c3a7572153c5a9acdcbf2a48700f79385678718f
matveev96 <sasha.matveev1996@icloud.com>
2025-02-07 18:10:56 +0300

    feat: add ThunkCreator, change AddTaskAC on addTaskTC

--- src/features/todolists/ui/Todolists/Todolist/Todolist.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Todolist.tsx
@@ -1,21 +1,21 @@
 import {AddItemForm} from "../../../../../common/components/AddItemForm/AddItemForm";
-import {TodolistType} from "../../../model/todolists-reducer";
-import {AddTaskAC} from "../../../model/tasks-reducer";
+import {DomainTodolist} from "../../../model/todolists-reducer";
+import {addTaskTC} from "../../../model/tasks-reducer";
 import {TodolistTitle} from "./TodolistTitle/TodolistTitle";
 import {FilterButtons} from "./FilterButtons/FilterButtons";
 import {Tasks} from "./Tasks/Tasks";
-import {useAppDispatch} from "../../../../../app/hooks";
+
+import {useAppDispatch} from "../../../../../common/hooks/useAppDispatch";
 
 type PropsType = {
-    todolist: TodolistType
+    todolist: DomainTodolist
 }
 
-export const Todolist = (props: PropsType) => {
-    const dispatch = useAppDispatch();
-    const {todolist} = props
+export const Todolist = ({todolist}: PropsType) => {
+    const dispatch = useAppDispatch()
 
     const addTask = (title: string) => {
-        dispatch(AddTaskAC({todolistId: todolist.id, title}))
+        dispatch(addTaskTC({ title, todolistId: todolist.id }))
     }
 
     return (
