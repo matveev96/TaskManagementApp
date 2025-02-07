dae64e8c4bdbc439b0eef18720270d9c11cea57e
matveev96 <sasha.matveev1996@icloud.com>
2025-02-07 18:15:10 +0300

    feat: add useEffect and update PropsType

--- src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -1,12 +1,16 @@
-import {useAppSelector} from "../../../../../../app/hooks";
+import {useAppSelector} from "../../../../../../common/hooks/useAppSelector";
 import {selectTasks} from "../../../../model/tasks-selectors";
 import List from "@mui/material/List";
 import {useAutoAnimate} from "@formkit/auto-animate/react";
-import {TodolistType} from "../../../../model/todolists-reducer";
+import {DomainTodolist} from "../../../../model/todolists-reducer";
 import {Task} from "./Task/Task";
+import {TaskStatus} from "../../../../../../common/enums";
+import {useAppDispatch} from "../../../../../../common/hooks/useAppDispatch";
+import {useEffect} from "react";
+import {fetchTasksTC} from "../../../../model/tasks-reducer";
 
 type Props = {
-    todolist: TodolistType
+    todolist: DomainTodolist
 }
 
 export const Tasks = ({todolist}: Props) => {
@@ -14,12 +18,18 @@ export const Tasks = ({todolist}: Props) => {
 
     const tasks = useAppSelector(selectTasks)
 
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchTasksTC(todolist.id))
+    }, [])
+
     const filterOfTasks = () => {
         switch (todolist.filter) {
             case "active":
-                return tasks[todolist.id].filter(task => !task.isDone);
+                return tasks[todolist.id].filter(task => task.status === TaskStatus.New);
             case "completed":
-                return tasks[todolist.id].filter(task => task.isDone);
+                return tasks[todolist.id].filter(task => task.status === TaskStatus.Completed);
             default:
                 return tasks[todolist.id]
         }
@@ -29,12 +39,12 @@ export const Tasks = ({todolist}: Props) => {
     return (
         <>
             {
-                tasksForTodolist.length === 0
+                tasksForTodolist?.length === 0
                     ? <p>Тасок нет</p>
                     : <List ref={listRef}>
-                        {tasksForTodolist.map((task) => {
+                        {tasksForTodolist?.map((task) => {
                             return (
-                                <Task key={task.id} task={task} todolistId={todolist.id}/>
+                                <Task key={task.id} task={task} todolist={todolist}/>
                             )
                         })}
                     </List>
--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,13 +1,20 @@
-import {useAppSelector} from "../../../../app/hooks";
-
+import {useAppSelector} from "../../../../common/hooks/useAppSelector";
 import {selectTodolists} from "../../model/todolists-selectors";
 import Paper from "@mui/material/Paper";
 import {Todolist} from "./Todolist/Todolist";
+import {useAppDispatch} from "../../../../common/hooks/useAppDispatch";
+import {useEffect} from "react";
+import {fetchTodolistsTC} from "../../model/todolists-reducer";
 
 
 export const Todolists = () => {
 
     const todolists = useAppSelector(selectTodolists)
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchTodolistsTC())
+    }, [])
 
     return (
         <>
