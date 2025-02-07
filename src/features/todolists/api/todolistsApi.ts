be96ae3599a7206ada3e8c588e7495a3e6f8eea0
matveev96 <sasha.matveev1996@icloud.com>
2025-02-07 18:20:56 +0300

    feat: add axios requests to project

---
+++ src/features/todolists/api/todolistsApi.ts
@@ -0,0 +1,20 @@
+
+import { Todolist } from "./todolistsApi.types"
+import {instance} from "../../../common/instance";
+import type {BaseResponse} from "../../../common/types";
+
+export const todolistsApi = {
+  getTodolists() {
+    return instance.get<Todolist[]>("todo-lists")
+  },
+  updateTodolist(payload: { id: string; title: string }) {
+    const { title, id } = payload
+    return instance.put<BaseResponse>(`todo-lists/${id}`, { title })
+  },
+  createTodolist(title: string) {
+    return instance.post<BaseResponse<{ item: Todolist }>>("todo-lists", { title })
+  },
+  deleteTodolist(id: string) {
+    return instance.delete<BaseResponse>(`todo-lists/${id}`)
+  },
+}
