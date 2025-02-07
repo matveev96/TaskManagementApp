f78299737c257f77cd529508c5545fff3f6da97e
matveev96 <sasha.matveev1996@icloud.com>
2025-02-07 18:05:18 +0300

    feat: add middleware-redux to project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
-import {combineReducers, compose, legacy_createStore as createStore} from 'redux'
+import {applyMiddleware, combineReducers, compose, legacy_createStore as createStore, type UnknownAction} from 'redux'
 import { tasksReducer } from '../features/todolists/model/tasks-reducer'
 import { todolistsReducer } from '../features/todolists/model/todolists-reducer'
 import {appReducer} from "./app-reducer";
+import {thunk, type ThunkAction, type ThunkDispatch} from "redux-thunk";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -20,12 +21,13 @@ const rootReducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 // непосредственно создаём store
-export const store = createStore(rootReducer, {}, composeEnhancers())
+export const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunk)))
 
 // определить автоматически тип всего объекта состояния
 export type RootState = ReturnType<typeof store.getState>
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>
+export type AppThunk = ThunkAction<void, RootState, unknown, UnknownAction>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
