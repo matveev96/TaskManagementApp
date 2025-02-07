f71ccc11327b485ff2667c49aa898b5acbe9b3b2
matveev96 <sasha.matveev1996@icloud.com>
2025-02-07 18:07:19 +0300

    feat: add ThunkCreator, change addTodolistAC on addTodolistTC

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,9 +1,9 @@
-import {useAppDispatch} from "./hooks";
-import {addTodolistAC} from "../features/todolists/model/todolists-reducer";
+import {addTodolistTC} from "../features/todolists/model/todolists-reducer";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid2";
 import {AddItemForm} from "../common/components/AddItemForm/AddItemForm";
 import {Todolists} from "../features/todolists/ui/Todolists/Todolists";
+import {useAppDispatch} from "../common/hooks/useAppDispatch";
 
 
 export const Main = () => {
@@ -11,7 +11,7 @@ export const Main = () => {
     const dispatch = useAppDispatch();
 
     const addTodoList = (title: string) => {
-        dispatch(addTodolistAC(title))
+        dispatch(addTodolistTC(title))
     }
     return (
         <Container fixed>
