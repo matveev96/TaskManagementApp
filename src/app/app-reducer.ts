0916a27636ce203d9bc57f1865e71c50c6b8d56e
matveev96 <sasha.matveev1996@icloud.com>
2025-02-14 20:04:34 +0300

    feat: add setAppStatusAC and setAppErrorAC

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,15 +1,18 @@
 const initialState = {
   themeMode: "light" as ThemeMode,
+  status: "idle" as RequestStatus,
+  error: null as string | null,
 }
 
 export const appReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
   switch (action.type) {
-    case "CHANGE_THEME": {
-      return {
-        ...state,
-        themeMode: action.payload,
-      }
-    }
+    case "CHANGE_THEME":
+      return { ...state, themeMode: action.payload }
+    case "SET_STATUS":
+      return { ...state, status: action.payload.status }
+    case "SET_ERROR":
+      return { ...state, error: action.payload.error }
+
     default:
       return state
   }
@@ -19,10 +22,27 @@ export const changeThemeAC = (themeMode: ThemeMode) => {
   return { type: "CHANGE_THEME", payload: themeMode } as const
 }
 
-export type ChangeThemeAT = ReturnType<typeof changeThemeAC>
+export const setAppStatusAC = (status: RequestStatus) => {
+  return {
+    type: "SET_STATUS",
+    payload: { status },
+  } as const
+}
 
-type ActionType = ChangeThemeAT
+export const setAppErrorAC = (error: string | null) => {
+  return {
+    type: "SET_ERROR",
+    payload: { error },
+  } as const
+}
+
+export type ChangeThemeAT = ReturnType<typeof changeThemeAC>
+export type SetAppStatusAT = ReturnType<typeof setAppStatusAC>
+export type SetAppErrorAT = ReturnType<typeof setAppErrorAC>
+
+type ActionType = ChangeThemeAT | SetAppStatusAT | SetAppErrorAT
 
 export type ThemeMode = "dark" | "light"
+export type RequestStatus = "idle" | "loading" | "succeeded" | "failed"
 
 type InitialStateType = typeof initialState
