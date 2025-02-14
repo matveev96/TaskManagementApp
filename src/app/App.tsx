ba2e793684fb9cf013c52a205a6949cf4aaa0420
matveev96 <sasha.matveev1996@icloud.com>
2025-02-14 20:04:53 +0300

    feat: add ErrorSnackBar to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,8 @@ import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import { selectTheme } from "./app-selectors"
 import { getTheme } from "common/theme/theme"
-import { Header } from "common/components/Header/Header"
 import { Main } from "./Main"
+import { ErrorSnackBar, Header } from "common/index"
 
 function App() {
   const themeMode = useAppSelector(selectTheme)
@@ -17,6 +17,7 @@ function App() {
       <CssBaseline />
       <Header />
       <Main />
+      <ErrorSnackBar />
     </ThemeProvider>
   )
 }
