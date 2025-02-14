63c570484c53e2e39f16b02506dd3202b08beae4
matveev96 <sasha.matveev1996@icloud.com>
2025-02-14 19:47:32 +0300

    refator: add new index in common

---
+++ src/common/index.ts
@@ -0,0 +1,5 @@
+export { ErrorSnackBar } from "./components/ErrorSnackBar/ErrorSnackBar"
+export { Header } from "./components/Header/Header"
+export { MenuButton } from "./components/MenuButton/MenuButton"
+export { EditableSpan } from "./components/EditableSpan/EditableSpan"
+export { AddItemForm } from "./components/AddItemForm/AddItemForm"
