3ee72a07e2d49a63d67ea439b976225aa6dde051
matveev96 <sasha.matveev1996@icloud.com>
2025-02-14 20:03:51 +0300

    feat: add selectStatus and selectError

--- src/app/app-selectors.ts
+++ src/app/app-selectors.ts
@@ -1,4 +1,6 @@
-import { ThemeMode } from "./app-reducer"
+import { type RequestStatus, ThemeMode } from "./app-reducer"
 import { RootState } from "./store"
 
 export const selectTheme = (state: RootState): ThemeMode => state.app.themeMode
+export const selectStatus = (state: RootState): RequestStatus => state.app.status
+export const selectError = (state: RootState): string | null => state.app.error
