646029ab2edf5af2989e31ea01ae726b4832b8be
matveev96 <sasha.matveev1996@icloud.com>
2025-02-21 17:29:26 +0300

    feat: realize navigate in Main

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -4,13 +4,31 @@ import Grid from "@mui/material/Grid2"
 import { AddItemForm } from "common/index"
 import { Todolists } from "../features/todolists/ui/Todolists"
 import { useAppDispatch } from "common/hooks/useAppDispatch"
+import { useEffect } from "react"
+import { Path } from "common/routing/Routing"
+import { useAppSelector } from "common/hooks"
+import { selectIsLoggedIn } from "../features/auth/model/authSelectors"
+import { useNavigate } from "react-router"
 
 export const Main = () => {
   const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+  const navigate = useNavigate()
 
   const addTodoList = (title: string) => {
     dispatch(addTodolistTC(title))
   }
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate(Path.Login)
+    }
+  }, [isLoggedIn])
+  // Второй вариант реализации роутинга при логинезации, который выпилили из документации ReactRouter. Для использования без ошибок нужно его вставить перед return компоненты Login
+  // if(isLoggedIn) {
+  //   return <Navigate to={Path.Main} />
+  // }
+
   return (
     <Container fixed>
       <Grid container sx={{ mt: "30px" }}>
