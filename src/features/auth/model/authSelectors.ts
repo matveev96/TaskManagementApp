efb6f248c3af5413bdd2e43257c27a500b260c09
matveev96 <sasha.matveev1996@icloud.com>
2025-02-21 17:24:04 +0300

    feat: realize auth selectors

---
+++ src/features/auth/model/authSelectors.ts
@@ -0,0 +1,4 @@
+import type { RootState } from "../../../app/store"
+
+export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedIn
+export const selectIsInitialized = (state: RootState) => state.auth.isInitialized
