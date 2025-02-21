4112316b883aafd1a563640a62260862dc20f366
matveev96 <sasha.matveev1996@icloud.com>
2025-02-21 17:23:05 +0300

    feat: add BrowserRouter from react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import App from "./app/App"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
+import { BrowserRouter } from "react-router"
+import App from "./app/App"
 
 const root = createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 )
 
 // If you want to start measuring performance in your app, pass a function
