fab9619d6313be3b499ef3a8373efaa6c24a2623
matveev96 <sasha.matveev1996@icloud.com>
2025-02-23 20:55:37 +0300

    feat: add TaskManagementApp route

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router"
+import { Navigate, Route, Routes } from "react-router"
 import { Main } from "../../app/Main"
 import { Login } from "../../features/auth/ui/Login/Login"
 import { Page404 } from "common/components/Page404/Page404"
 
 export const Path = {
   Main: "/",
+  TaskManagementApp: "TaskManagementApp",
   Login: "login",
   NotFound: "*",
 } as const
@@ -13,6 +14,7 @@ export const Routing = () => {
   return (
     <Routes>
       <Route path={Path.Main} element={<Main />} />
+      <Route path={Path.TaskManagementApp} element={<Navigate to={Path.Main} />} />
       <Route path={Path.Login} element={<Login />} />
       <Route path={Path.NotFound} element={<Page404 />} />
     </Routes>
