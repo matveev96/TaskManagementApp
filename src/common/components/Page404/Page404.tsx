4bcfc51ef3fde4c0c7e079f208707a8a0ad6cef7
matveev96 <sasha.matveev1996@icloud.com>
2025-02-23 20:56:15 +0300

    feat: add Button Go Home to error404

--- src/common/components/Page404/Page404.tsx
+++ src/common/components/Page404/Page404.tsx
@@ -1,10 +1,16 @@
 import s from "./Page404.module.css"
+import Button from "@mui/material/Button"
+import { Link } from "react-router"
+import { Path } from "common/routing/Routing"
 
 export const Page404 = () => {
   return (
-    <>
+    <div className={s.page404}>
       <h1 className={s.title}>404</h1>
       <h2 className={s.subTitle}>page not found</h2>
-    </>
+      <Button sx={{ width: "150px" }} variant={"outlined"} size={"large"} component={Link} to={Path.Main}>
+        Go Home
+      </Button>
+    </div>
   )
 }
