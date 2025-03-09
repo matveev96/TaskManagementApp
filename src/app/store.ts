9f5b148c28aa02dc97cc3bc58788c8db4a4d21a1
matveev96 <sasha.matveev1996@icloud.com>
2025-03-09 21:26:25 +0300

    feat: add baseApi from RTK-Query to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,19 @@
-import { type UnknownAction } from "redux"
-import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice"
-import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
 import { appReducer, appSlice } from "./appSlice"
-import { type ThunkAction, type ThunkDispatch } from "redux-thunk"
-import { authReducer, authSlice } from "../features/auth/model/authSlice"
 import { configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { baseApi } from "./baseApi"
+import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
 
 export const store = configureStore({
   reducer: {
-    [tasksSlice.name]: tasksReducer,
-    [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
-    [authSlice.name]: authReducer,
+    [todolistsSlice.name]: todolistsReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 // определить автоматически тип всего объекта состояния
 export type RootState = ReturnType<typeof store.getState>
