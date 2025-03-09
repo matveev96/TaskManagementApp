add951a66d3fdcfee4745206710066f4044838f7
matveev96 <sasha.matveev1996@icloud.com>
2025-03-09 21:27:33 +0300

    refactor: add useGetTodolistsQuery from RTK-Query

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,21 +1,13 @@
-import { useAppSelector } from "common/hooks/useAppSelector"
 import Paper from "@mui/material/Paper"
 import { Todolist } from "./Todolist"
-import { useAppDispatch } from "common/hooks/useAppDispatch"
-import { useEffect } from "react"
-import { fetchTodolistsTC, selectTodolists } from "../../model/todolistsSlice"
+import { useGetTodolistsQuery } from "../../api/todolistsApi"
 
 export const Todolists = () => {
-  const todolists = useAppSelector(selectTodolists)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchTodolistsTC())
-  }, [])
+  const { data } = useGetTodolistsQuery()
 
   return (
     <>
-      {todolists.map((el) => {
+      {data?.map((el) => {
         return (
           <Paper key={el.id} elevation={4} sx={{ p: "0 20px 20px 20px" }}>
             <Todolist todolist={el} />
