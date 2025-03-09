bcc88892369eebab6cec1ec0c3ee6fb3efe3f1e1
matveev96 <sasha.matveev1996@icloud.com>
2025-03-09 21:25:23 +0300

    refactor: add todolistsApi from RTK-Query

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,19 +1,42 @@
 import { Todolist } from "./todolistsApi.types"
-import { instance } from "common/instance"
 import type { BaseResponse } from "common/types"
+import type { DomainTodolist } from "../model/todolistsSlice"
+import { baseApi } from "../../../app/baseApi"
 
-export const todolistsApi = {
-  getTodolists() {
-    return instance.get<Todolist[]>("todo-lists")
-  },
-  updateTodolist(payload: { id: string; title: string }) {
-    const { title, id } = payload
-    return instance.put<BaseResponse>(`todo-lists/${id}`, { title })
-  },
-  createTodolist(title: string) {
-    return instance.post<BaseResponse<{ item: Todolist }>>("todo-lists", { title })
-  },
-  deleteTodolist(id: string) {
-    return instance.delete<BaseResponse>(`todo-lists/${id}`)
-  },
-}
+export const todolistsApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getTodolists: build.query<DomainTodolist[], void>({
+      query: () => "todo-lists",
+      transformResponse: (todolists: Todolist[]) => {
+        return todolists.map((tl) => ({ ...tl, filter: "all", entityStatus: "idle" }))
+      },
+      providesTags: ["Todolist"],
+    }),
+    createTodolist: build.mutation<BaseResponse<{ item: Todolist }>, string>({
+      query: (title) => ({
+        method: "POST",
+        url: "todo-lists",
+        body: { title },
+      }),
+      invalidatesTags: ["Todolist"],
+    }),
+    deleteTodolist: build.mutation<BaseResponse, string>({
+      query: (id) => ({
+        method: "DELETE",
+        url: `todo-lists/${id}`,
+      }),
+      invalidatesTags: ["Todolist"],
+    }),
+    updateTodolist: build.mutation<BaseResponse, { id: string; title: string }>({
+      query: ({ title, id }) => ({
+        method: "PUT",
+        url: `todo-lists/${id}`,
+        body: { title },
+      }),
+      invalidatesTags: ["Todolist"],
+    }),
+  }),
+})
+
+export const { useGetTodolistsQuery, useCreateTodolistMutation, useDeleteTodolistMutation, useUpdateTodolistMutation } =
+  todolistsApi
