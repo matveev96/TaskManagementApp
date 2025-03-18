bed49f5edede0080858b99044b5e649df639e821
matveev96 <sasha.matveev1996@icloud.com>
2025-03-18 18:11:43 +0300

    feat: add updateEntityStatus in removeToDoList

--- src/features/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
@@ -3,35 +3,42 @@ import { EditableSpan } from "common/index"
 import IconButton from "@mui/material/IconButton"
 import DeleteIcon from "@mui/icons-material/Delete"
 import Box from "@mui/material/Box"
-import { DomainTodolist } from "../../../../model/todolistsSlice"
 
 import { useDeleteTodolistMutation, useUpdateTodolistMutation } from "../../../../api/todolistsApi"
+import { useAppDispatch } from "common/hooks"
+import type { DomainTodolist } from "../../../../lib/types"
+import { updateEntityStatus } from "../utils/updateQueryEntityStatus"
 
 type Props = {
   todolist: DomainTodolist
 }
 
 export const TodolistTitle = ({ todolist }: Props) => {
+  const { id, entityStatus, title } = todolist
+
   const [deleteTodolist] = useDeleteTodolistMutation()
   const [updateTodolist] = useUpdateTodolistMutation()
 
+  const dispatch = useAppDispatch()
+
   const changeTodoListTitle = (updatedTitle: string) => {
-    updateTodolist({ title: updatedTitle, id: todolist.id })
+    updateTodolist({ title: updatedTitle, id })
   }
 
   const removeToDoList = () => {
-    deleteTodolist(todolist.id)
+    updateEntityStatus({ status: "loading", dispatch, id })
+    deleteTodolist(id)
+      .unwrap()
+      .catch(() => {
+        updateEntityStatus({ status: "idle", dispatch, id })
+      })
   }
   return (
     <Box sx={TitleTodolistSx}>
       <h3>
-        <EditableSpan
-          oldTitle={todolist.title}
-          onClick={changeTodoListTitle}
-          disabled={todolist.entityStatus === "loading"}
-        />
+        <EditableSpan oldTitle={title} onClick={changeTodoListTitle} disabled={entityStatus === "loading"} />
       </h3>
-      <IconButton onClick={removeToDoList} disabled={todolist.entityStatus === "loading"}>
+      <IconButton onClick={removeToDoList} disabled={entityStatus === "loading"}>
         <DeleteIcon />
       </IconButton>
     </Box>
