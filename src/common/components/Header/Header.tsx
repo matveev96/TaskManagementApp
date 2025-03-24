8fbe360f0366b969290415ff817c44a62d323ef4
matveev96 <sasha.matveev1996@icloud.com>
2025-03-24 12:53:58 +0300

    feat: realize adaptive design to Header and add SwitchDarkMode component

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,32 +1,22 @@
 import Toolbar from "@mui/material/Toolbar"
-import { getToolbar } from "./Header.styles"
-import IconButton from "@mui/material/IconButton"
-import MenuIcon from "@mui/icons-material/Menu"
-import Switch from "@mui/material/Switch"
 import AppBar from "@mui/material/AppBar"
-import { selectIsLoggedIn, selectStatus, selectTheme, setChangeTheme, setIsLoggedIn } from "../../../app/appSlice"
+import { selectIsLoggedIn, selectStatus, selectTheme, setIsLoggedIn, setModalFAQ } from "../../../app/appSlice"
 import { useAppSelector } from "common/hooks"
-import { getTheme } from "common/theme"
 import { useAppDispatch } from "common/hooks"
 import { LinearProgress } from "@mui/material"
 import { MenuButton } from "common/index"
 import { useLogoutMutation } from "../../../features/auth/api/authApi"
 import { ResultCode } from "common/enums"
 import { baseApi } from "../../../app/baseApi"
+import { SwitchDarkMode } from "common/components/SwithDarkMode/SwitchDarkMode"
+import { getTheme } from "common/theme"
+import Box from "@mui/material/Box"
 
 export const Header = () => {
-  const themeMode = useAppSelector(selectTheme)
   const status = useAppSelector(selectStatus)
   const dispatch = useAppDispatch()
-  const theme = getTheme(themeMode)
-
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
-
   const [logout] = useLogoutMutation()
-
-  const changeModeHandler = () => {
-    dispatch(setChangeTheme({ themeMode: themeMode === "light" ? "dark" : "light" }))
-  }
 
   const logoutHandler = () => {
     logout()
@@ -41,17 +31,38 @@ export const Header = () => {
       })
   }
 
+  const changeModalFAQHandler = () => {
+    dispatch(setModalFAQ({ modalFAQ: true }))
+  }
+
+  const themeMode = useAppSelector(selectTheme)
+  const theme = getTheme(themeMode)
+
   return (
     <AppBar position="static" sx={{ mb: "30px" }}>
-      <Toolbar sx={getToolbar}>
-        <IconButton color="inherit">
-          <MenuIcon />
-        </IconButton>
-        <div>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          [theme.breakpoints.down(565)]: {
+            justifyContent: "center",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            [theme.breakpoints.down(565)]: {
+              display: "none",
+            },
+          }}
+        >
+          Task Management App
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           {isLoggedIn && <MenuButton onClick={logoutHandler}>Logout</MenuButton>}
-          <MenuButton background={theme.palette.primary.dark}>FAQ</MenuButton>
-          <Switch color={"default"} onChange={changeModeHandler} />
-        </div>
+          <MenuButton onClick={changeModalFAQHandler}>FAQ</MenuButton>
+          <SwitchDarkMode />
+        </Box>
       </Toolbar>
       {status === "loading" && <LinearProgress />}
     </AppBar>
