c45a52de977223a97d6589c4c7db4ce4d0c0c9fa
matveev96 <sasha.matveev1996@icloud.com>
2025-03-24 12:43:01 +0300

    feat: add text and styles to empty window with Todolists

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,6 +2,7 @@ import Paper from "@mui/material/Paper"
 import { Todolist } from "./Todolist"
 import { useGetTodolistsQuery } from "../../api/todolistsApi"
 import { TodolistSkeleton } from "../skeletons/TodolistSkeleton/TodolistSkeleton"
+import s from "./todolists.module.css"
 
 export const Todolists = () => {
   const { data, isLoading } = useGetTodolistsQuery()
@@ -19,13 +20,20 @@ export const Todolists = () => {
   }
   return (
     <>
-      {data?.map((el) => {
-        return (
-          <Paper key={el.id} elevation={4} sx={{ p: "0 20px 20px 20px" }}>
-            <Todolist todolist={el} />
-          </Paper>
-        )
-      })}
+      {data?.length ? (
+        data.map((el) => {
+          return (
+            <Paper key={el.id} elevation={4} sx={{ p: "0 20px 20px 20px" }}>
+              <Todolist todolist={el} />
+            </Paper>
+          )
+        })
+      ) : (
+        <div className={s.noTodolists}>
+          <h1 className={s.title}>No Todolists here</h1>
+          <h2 className={s.subTitle}>Add new Todolist 📝</h2>
+        </div>
+      )}
     </>
   )
 }
